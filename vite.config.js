224b9c58a823cef042247c2c25a74293e281652c
jabir756 <jabirkagone6@gmail>
2025-01-19 12:05:41 -0500

    update base path for the project to reflect new naming

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/react-todo-app/'
+  base: '/react-to-do-list/'
 });
